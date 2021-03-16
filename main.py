6f85e55025f5b191f187e44065fdfa73d828146e
jquiroz111 <62913410+jquiroz111@users.noreply.github.com>
2021-03-16 15:48:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ from flask import Flask, jsonify, escape, request, Response
 import random
 import hashlib
 import math
+
+# instansiate the Flask object
+app = Flask(__name__)
+
+# the main route url route
+@app.route('/')
+def hello():
+    return "Hello, Welcome to group project #5."
