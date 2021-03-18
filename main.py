666df824b84b0d3bb260cf2d656d897918713f1b
yehseph <78126316+yehseph@users.noreply.github.com>
2021-03-18 12:36:09 -0500

    Factorial calculation

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ def json_response():
     resp.headers['Content-Type'] = 'application/json'
     return resp
 '''
+# Calculation for factorials
+def factorial():
+    n = int(input("enter a number:"))
+    factorial = 1
+    if n == 0:
+        print("The factorial of 0 is 1")
+    else:
+       for i in range(1,n + 1):
+           factorial = factorial*i
+       print("The factorial of number:",factorial)  
